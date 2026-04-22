a530172c1949cd14bf4999bb16be236cdf4d743f
Himanshu Anand <himanshuanand480@gmail.com>
2026-04-23 00:52:55 +0530

    Added PDF loader module

---
+++ src/backend/pdf_loader.py
@@ -0,0 +1,28 @@
+import fitz   # PyMuPDF
+
+def extract_text_from_pdf(file_path):
+    doc = fitz.open(file_path)
+    text = ""
+
+    for page in doc:
+        text += page.get_text()
+
+    doc.close()   # IMPORTANT: close file
+    return text
+
+
+def extract_pagewise(file_path):
+    doc = fitz.open(file_path)
+    data = []
+
+    for page_num in range(len(doc)):
+        page = doc[page_num]
+        text = page.get_text()
+
+        data.append({
+            "page": page_num + 1,
+            "content": text
+        })
+
+    doc.close()   # IMPORTANT
+    return data
